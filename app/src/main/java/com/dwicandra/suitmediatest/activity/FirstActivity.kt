f1a7052f21053dbff90642d1b1a26eb2e0e4747c
cekakaka <dwicandra920@gmail.com>
2022-07-10 12:00:46 +0700

    Initial commit

---
+++ app/src/main/java/com/dwicandra/suitmediatest/activity/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.dwicandra.suitmediatest.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dwicandra.suitmediatest.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
